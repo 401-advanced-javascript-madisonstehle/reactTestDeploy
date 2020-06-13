6061777ba7b1e4fcdda4d746aeb55caffb8d6f25
Madison Stehle <mnstehle3@gmail.com>
2020-06-13 10:33:01 -0700

    installed sass, wrote C Counter tests for buttons

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -7,8 +7,6 @@ configure({ adapter: new Adapter() });
 import { shallow, mount } from 'enzyme';
 import Counter from '../components/Counter.js';
 
-// On clicking the - button, the state count variable should be decreased by 1
-// On clicking the + button, the state count variable should be increased by 1
 // When the state count variable changes, the HTML should update and match that change
 
 describe('functionality', () => {
@@ -19,9 +17,35 @@ describe('functionality', () => {
 
   it('add', () => {
     let component = shallow(<Counter />);
-    let fakeEvent = { target: { count: 0 } };
-    component.find('.up clicker').simulate('click', fakeEvent);
+    let fakeEvent = {};
+    component.find('span.up.clicker').simulate('click', fakeEvent);
     expect(component.state('count')).toBe(1);
+    expect(component.state('polarity')).toBe('positive');
+  });
+
+  it('subtract', () => {
+    let component = shallow(<Counter />);
+    let fakeEvent = {};
+    component.find('span.down.clicker').simulate('click', fakeEvent);
+    expect(component.state('count')).toBe(-1);
+    expect(component.state('polarity')).toBe('negative');
+  });
+
+  it('toggle between positive and negative', () => {
+    let component = shallow(<Counter />);
+    let fakeEvent = {};
+
+    component.find('span.down.clicker').simulate('click', fakeEvent);
+    expect(component.state('count')).toBe(-1);
+    expect(component.state('polarity')).toBe('negative');
+
+    component.find('span.up.clicker').simulate('click', fakeEvent);
+    expect(component.state('count')).toBe(0);
+    expect(component.state('polarity')).toBe('');
+
+    component.find('span.up.clicker').simulate('click', fakeEvent);
+    expect(component.state('count')).toBe(1);
+    expect(component.state('polarity')).toBe('positive');
   });
 });
 
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Counter from './components/Counter.js';
 
+import './styles/reset.css';
+import './styles/styles.scss';
+
 class App extends React.Component {
     render() {
         return (
